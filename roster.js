203c3e17653c5a0b116f125a65490847c31ed0d1
CLAY-HMS <geisenmann2005@gmail.com>
2025-02-14 17:51:44 +0000

    push

--- roster.js
+++ roster.js
@@ -6,10 +6,21 @@ function()
     for (let i = 0; i < statsButtons.length; i++) 
     {
         const currentButton = statsButtons[i];
-        currentButton.addEventListener
-        ('click', function() {
+        currentButton.addEventListener('click', 
+        function() {
         const targetId = this.dataset.target;
         const statsRow = document.getElementById(targetId);
+
+        const allStatsRows = document.quarrySelectorAll('.stats-row'); // For all stats row
+
+        for(let i =0; i < allStatsRows.length; i++) //  looks through all the rows and closes them before the next row is open 
+        {
+            const currentRow = allStatsRows[i];
+            if(currentRow !== statsRow && currentRow.classList.contains('show'))
+            {
+                statsRow.classList.remove('show');
+            }
+        }
 
         if (statsRow) 
             {
