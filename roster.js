02c2695a8a95a08f587e5e852a360236fc95a3f7
CLAY-HMS <geisenmann2005@gmail.com>
2025-02-14 22:56:05 +0000

    finished roster

--- roster.js
+++ roster.js
@@ -1,38 +1,22 @@
-document.addEventListener('DOMContentLoaded', 
-function() 
-{
+document.addEventListener('DOMContentLoaded', function() {
     const statsButtons = document.querySelectorAll('.stats-button');
-    
-    for (let i = 0; i < statsButtons.length; i++) 
-    {
-        const currentButton = statsButtons[i];
-        currentButton.addEventListener('click', 
-        function() {
-        const targetId = this.dataset.target;
-        const statsRow = document.getElementById(targetId);
 
-        const allStatsRows = document.quarrySelectorAll('.stats-row'); // For all stats row
+    statsButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const targetId = this.dataset.target;
+            const statsRow = document.getElementById(targetId);
 
-        for(let i =0; i < allStatsRows.length; i++) //  looks through all the rows and closes them before the next row is open 
-        {
-            const currentRow = allStatsRows[i];
-            if(currentRow !== statsRow && currentRow.classList.contains('show'))
-            {
-                statsRow.classList.remove('show');
-            }
-        }
-
-        if (statsRow) 
-            {
-                if (statsRow.classList.contains('show')) 
-                {
-                    statsRow.classList.remove('show');
-                } 
-                else 
-                {
-                statsRow.classList.add('show');
+          
+            document.querySelectorAll('.stats-row.show').forEach(row => {
+                if (row !== statsRow) {
+                    row.classList.remove('show');
                 }
+            });
+
+           
+            if (statsRow) {
+                statsRow.classList.toggle('show');
             }
         });
-    }
+    });
 });
